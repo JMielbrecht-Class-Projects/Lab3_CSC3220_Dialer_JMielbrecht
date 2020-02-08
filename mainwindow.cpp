62a7ad3c539586f6a54b075d4ca95253bd2cbe21
banksrjr <60370192+banksrjr@users.noreply.github.com>
2020-02-07 18:10:33 -0800

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,6 +10,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     ui->tableView->setModel(myModel);
+    ui->zeroButton->textArea.readOnly = true;
 }
 
 MainWindow::~MainWindow()
