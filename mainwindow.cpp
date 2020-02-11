eb3f1be510bcd9b8d144572c6736cc2399a649c5
Jordan Mielbrecht <mielbrechtj@spu.edu>
2020-02-10 22:58:52 -0800

    Added phoneNumber; Button implementation
    QString phoneNumber added
    on_clicked() functions implemented for buttons, appending their respective digits to phoneNumber

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -45,3 +45,73 @@ void MainWindow::loadImage(){
     }
     ui->phoneIcon->setPixmap(imagePhone);
 }
+
+void MainWindow::on_zeroButton_clicked()
+{
+    while(phoneNumber.length() <= 9){
+        phoneNumber.append("0");
+    }
+}
+
+void MainWindow::on_nineButton_clicked()
+{
+    while(phoneNumber.length() <= 9){
+        phoneNumber.append("9");
+    }
+}
+
+void MainWindow::on_eightButton_clicked()
+{
+    while(phoneNumber.length() <= 9){
+        phoneNumber.append("8");
+    }
+}
+
+void MainWindow::on_sevenButton_clicked()
+{
+    while(phoneNumber.length() <= 9){
+        phoneNumber.append("7");
+    }
+}
+
+void MainWindow::on_sixButton_clicked()
+{
+    while(phoneNumber.length() <= 9){
+        phoneNumber.append("6");
+    }
+}
+
+void MainWindow::on_fiveButton_clicked()
+{
+    while(phoneNumber.length() <= 9){
+        phoneNumber.append("5");
+    }
+}
+
+void MainWindow::on_fourButton_clicked()
+{
+    while(phoneNumber.length() <= 9){
+        phoneNumber.append("4");
+    }
+}
+
+void MainWindow::on_threeButton_clicked()
+{
+    while(phoneNumber.length() <= 9){
+        phoneNumber.append("3");
+    }
+}
+
+void MainWindow::on_twoButton_clicked()
+{
+    while(phoneNumber.length() <= 9){
+        phoneNumber.append("2");
+    }
+}
+
+void MainWindow::on_oneButton_clicked()
+{
+    while(phoneNumber.length() <= 9){
+        phoneNumber.append("1");
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -21,10 +21,31 @@ private slots:
 
     void on_tableView_clicked(const QModelIndex &index);
 
+    void on_zeroButton_clicked();
+
+    void on_nineButton_clicked();
+
+    void on_eightButton_clicked();
+
+    void on_sevenButton_clicked();
+
+    void on_sixButton_clicked();
+
+    void on_fiveButton_clicked();
+
+    void on_fourButton_clicked();
+
+    void on_threeButton_clicked();
+
+    void on_twoButton_clicked();
+
+    void on_oneButton_clicked();
+
 private:
     Ui::MainWindow *ui;
     PhoneAddressBookModel *myModel;
     QPixmap imagePhone;
+    QString phoneNumber;
 
     void loadImage();
 };
